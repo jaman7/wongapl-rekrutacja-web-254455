e43bc3cc1f5cc0d96618cd845c74517a692424c0
jaman7 <krzysztofbalon123@gmail.com>
2025-06-12 01:10:04 +0200

    footer section added

--- tina/collections/global-config.ts
+++ tina/collections/global-config.ts
@@ -107,6 +107,37 @@ export const GlobalConfigCollection: Collection = {
       isBody: true,
       parser: { type: 'mdx' },
     },
+    {
+      name: 'footer',
+      label: 'Footer Columns',
+      type: 'object',
+      fields: [
+        {
+          name: 'columns',
+          label: 'Columns',
+          type: 'object',
+          list: true,
+          fields: [
+            {
+              name: 'title',
+              type: 'string',
+              label: 'Column Title',
+              required: true,
+            },
+            {
+              name: 'links',
+              label: 'Links',
+              type: 'object',
+              list: true,
+              fields: [
+                { name: 'title', type: 'string', label: 'Link Title', required: true },
+                { name: 'link', type: 'string', label: 'Link URL or Text', required: true },
+              ],
+            },
+          ],
+        },
+      ],
+    },
 
     // Add other config fields here...
   ],
