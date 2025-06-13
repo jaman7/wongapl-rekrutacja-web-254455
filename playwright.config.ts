52d6193b8106bbf2ac16723ce2fee0acb9be704b
jaman7 <krzysztofbalon123@gmail.com>
2025-06-13 09:38:55 +0200

    e2e lighthouse repair empty page e2e 3

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: './tests',
   timeout: 10000,
   retries: 0,
-  reporter: [['html']],
+  reporter: [['html', { outputFolder: 'playwright-report/html-report', open: 'never' }]],
   use: {
     headless: true,
     baseURL: 'https://jaman7-wongapl-rekrutacja-web-254455.pages.dev',
